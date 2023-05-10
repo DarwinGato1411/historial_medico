246f2c2a886d25a7be0cfe7fa652cf2854f08163
NanoDan29 <jowell29jm@gmail.com>
2023-05-10 14:59:59 -0500

    correccion campos tabla vista meidca

--- HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Receta_.java
+++ HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Receta_.java
@@ -5,13 +5,17 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-10T09:53:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-10T13:48:02")
 @StaticMetamodel(Receta.class)
 public class Receta_ { 
 
+    public static volatile SingularAttribute<Receta, Boolean> recT;
     public static volatile SingularAttribute<Receta, String> recDescripcion;
     public static volatile SingularAttribute<Receta, VisitaMedica> idVisitaMedica;
     public static volatile SingularAttribute<Receta, String> recMedicamento;
     public static volatile SingularAttribute<Receta, Integer> idReceta;
+    public static volatile SingularAttribute<Receta, Boolean> recM;
+    public static volatile SingularAttribute<Receta, Boolean> recN;
+    public static volatile SingularAttribute<Receta, String> recCantidad;
 
 }
