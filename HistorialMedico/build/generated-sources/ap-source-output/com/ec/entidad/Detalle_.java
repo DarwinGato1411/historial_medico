bdbcd52981222a151bfb454bedba76f8ac35969c
NanoDan29 <jowell29jm@gmail.com>
2023-05-16 10:02:10 -0500

    correccion reportes

--- HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Detalle_.java
+++ HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Detalle_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-10T21:02:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-15T16:27:37")
 @StaticMetamodel(Detalle.class)
 public class Detalle_ { 
 
