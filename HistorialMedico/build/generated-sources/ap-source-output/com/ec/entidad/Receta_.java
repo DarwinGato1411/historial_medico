fd888882f1eeb38e24e63d94a88eba79cdbe79a4
DarwinGato1411 <darwinvinicio14@gmail.com>
2026-06-23 15:00:46 -0500

    cambios Drawin

--- HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Receta_.java
+++ HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/Receta_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-19T21:03:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-04T11:09:21")
 @StaticMetamodel(Receta.class)
 public class Receta_ { 
 
--- HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/VisitaMedica_.java
+++ HistorialMedico/build/generated-sources/ap-source-output/com/ec/entidad/VisitaMedica_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-19T21:03:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-04T11:09:21")
 @StaticMetamodel(VisitaMedica.class)
 public class VisitaMedica_ { 
 
